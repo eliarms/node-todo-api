d22d67d9c8d56fe6319e5c1d7b962941ea26dd63
Emmanuel Eliason-Armstrong <geteliarms@gmail.com>
2021-01-18 19:03:20 +0700

    Added docker compose file & code refractoring

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,29 @@
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+  useCreateIndex: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  connectTimeoutMS: 10000,
+};
+
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+
 var mongoose = require('mongoose');
 //initate the connection string
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp',{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+//mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp',{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
   //Instantiate the TodoApp DB
 //var db = client.db('TodoApp');
-
+mongoose.connect(url, options);
 module.exports ={mongoose};
 
