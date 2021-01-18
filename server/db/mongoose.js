eca88cea2fcedfea1a26c8a55c63be26c916f7e3
Emmanuel Eliason-Armstrong <geteliarms@gmail.com>
2021-01-18 16:35:31 +0700

    Fix security vulnerabilities and update mongodb conection string parameters

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 //initate the connection string
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp',{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
   //Instantiate the TodoApp DB
 //var db = client.db('TodoApp');
 
 module.exports ={mongoose};
+
